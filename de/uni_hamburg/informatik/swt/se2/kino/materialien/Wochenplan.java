0fed1afdaff8342d815f47a7d360917d0792209c
Jim Martens <jim1@live.de>
2013-06-16 18:07:37 +0200

    Added missing add/removal functionality for Wochenplan.

--- de/uni_hamburg/informatik/swt/se2/kino/materialien/Wochenplan.java
+++ de/uni_hamburg/informatik/swt/se2/kino/materialien/Wochenplan.java
@@ -1,5 +1,11 @@
 package de.uni_hamburg.informatik.swt.se2.kino.materialien;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Datum;
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Tag;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Woche;
 
 /**
@@ -13,6 +19,7 @@ public class Wochenplan
 {
     private Woche _woche;
     private Kinosaal _kinosaal;
+    private Map<Datum,Tagesplan> _tagesplaene;
     
     /**
      * Initialisiert einen Wochenplan.
@@ -32,6 +39,15 @@ public class Wochenplan
         assert kinosaal != null : "Vorbedingung verletzt: kinosaal != null";
         _woche = woche;
         _kinosaal = kinosaal;
+        List<Tag> wochentage = _woche.getWochentage();
+        _tagesplaene = new HashMap<>(7);
+        _tagesplaene.put(wochentage.get(0).getDatum(), new Tagesplan(wochentage.get(0).getDatum()));
+        _tagesplaene.put(wochentage.get(1).getDatum(), new Tagesplan(wochentage.get(1).getDatum()));
+        _tagesplaene.put(wochentage.get(2).getDatum(), new Tagesplan(wochentage.get(2).getDatum()));
+        _tagesplaene.put(wochentage.get(3).getDatum(), new Tagesplan(wochentage.get(3).getDatum()));
+        _tagesplaene.put(wochentage.get(4).getDatum(), new Tagesplan(wochentage.get(4).getDatum()));
+        _tagesplaene.put(wochentage.get(5).getDatum(), new Tagesplan(wochentage.get(5).getDatum()));
+        _tagesplaene.put(wochentage.get(6).getDatum(), new Tagesplan(wochentage.get(6).getDatum()));
     }
     
     /**
@@ -52,5 +68,78 @@ public class Wochenplan
     public Kinosaal getKinosaal()
     {
         return _kinosaal;
+    }
+    
+    /**
+     * Fügt eine Vorstellung in den Wochenplan ein.
+     * 
+     * @param vorstellung Die einzufügende Vorstellung.
+     * 
+     * @require !istVorhanden(vorstellung)
+     * 
+     * @ensure istVorhanden(vorstellung)
+     */
+    public void fuegeVorstellungHinzu(Vorstellung vorstellung)
+    {
+        assert !istVorhanden(vorstellung) : "Vorbedingung verletzt: !istVorhanden(vorstellung)";
+        Datum datum = vorstellung.getDatum();
+        _tagesplaene.get(datum).fuegeVorstellungHinzu(vorstellung);
+    }
+    
+    /**
+     * Entfernt die angegebene Vorstellung vom Wochenplan.
+     * 
+     * @param vorstellung Die zu entfernende Vorstellung.
+     * 
+     * @require istVorhanden(vorstellung)
+     * 
+     * @ensure !istVorhanden(vorstellung)
+     */
+    public void entferneVorstellung(Vorstellung vorstellung)
+    {
+        assert istVorhanden(vorstellung) : "Vorbedingung verletzt: istVorhanden(vorstellung)";
+        Datum datum = vorstellung.getDatum();
+        _tagesplaene.get(datum).entferneVorstellung(vorstellung);
+    }
+    
+    /**
+     * Prüft, ob eine Vorstellung im Wochenplan vorhanden ist.
+     * 
+     * @param vorstellung Die zu überprüfende Vorstellung.
+     * 
+     * @require vorstellung != null
+     */
+    public boolean istVorhanden(Vorstellung vorstellung)
+    {
+        assert vorstellung != null : "Vorbedingung verletzt: vorstellung != null";
+        Datum datum = vorstellung.getDatum();
+        List<Vorstellung> vorstellungen = _tagesplaene.get(datum).getVorstellungen();
+        
+        return vorstellungen.contains(vorstellung);
+    }
+    
+    /**
+     * Prüft ob das gegebene Datum in diesem Wochenplan vorkommt.
+     * 
+     * @param datum
+     * @return <code>true</code> wenn das Datum vorkommt,
+     *          <code>false</code> wenn nicht
+     * 
+     * @require datum != null
+     */
+    public boolean istTagInDiesemPlan(Datum datum)
+    {
+        assert datum != null : "Vorbedingung verletzt: datum != null";
+        boolean result = false;
+        List<Tag> wochentage = _woche.getWochentage();
+        for (Tag tag : wochentage)
+        {
+            if (tag.getDatum().equals(datum))
+            {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
--- de/uni_hamburg/informatik/swt/se2/kino/materialien/WochenplanTest.java
+++ de/uni_hamburg/informatik/swt/se2/kino/materialien/WochenplanTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Datum;
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.FSK;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Reinigungszeit;
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Uhrzeit;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Woche;
 
 /**
@@ -17,12 +20,18 @@ public class WochenplanTest
 {
     private Woche _woche;
     private Kinosaal _kinosaal;
+    private Vorstellung _vorstellung;
     
     @Before
     public void setUp()
     {
         _woche = Woche.dieseWoche();
         _kinosaal = new Kinosaal("Kino1", 10, 10, new Reinigungszeit(15));
+        Film film = new Film("Hammersmith", 90, FSK.FSK12, false, false);
+        Uhrzeit anfang = new Uhrzeit(17, 30);
+        Uhrzeit ende = new Uhrzeit(20, 00);
+        Datum datum = _woche.getWochentage().get(0).getDatum();
+        _vorstellung = new Vorstellung(_kinosaal, film, anfang, ende, datum, 750);
     }
     
     @Test
@@ -33,4 +42,32 @@ public class WochenplanTest
         assertSame(_kinosaal, wochenplan.getKinosaal());
     }
     
+    @Test
+    public void testIstTagInDiesemPlan()
+    {
+        Wochenplan wochenplan = new Wochenplan(_woche, _kinosaal);
+        Datum datumVorhanden = _woche.getWochentage().get(0).getDatum();
+        Datum datumNichtVorhanden = new Datum(1, 1, 1909);
+        assertTrue(wochenplan.istTagInDiesemPlan(datumVorhanden));
+        assertFalse(wochenplan.istTagInDiesemPlan(datumNichtVorhanden));
+    }
+    
+    @Test
+    public void testFuegeVorstellungHinzu()
+    {
+        Wochenplan wochenplan = new Wochenplan(_woche, _kinosaal);
+        assertFalse(wochenplan.istVorhanden(_vorstellung));
+        wochenplan.fuegeVorstellungHinzu(_vorstellung);
+        assertTrue(wochenplan.istVorhanden(_vorstellung));
+    }
+    
+    @Test
+    public void testEntferneVorstellung()
+    {
+        Wochenplan wochenplan = new Wochenplan(_woche, _kinosaal);
+        wochenplan.fuegeVorstellungHinzu(_vorstellung);
+        assertTrue(wochenplan.istVorhanden(_vorstellung));
+        wochenplan.entferneVorstellung(_vorstellung);
+        assertFalse(wochenplan.istVorhanden(_vorstellung));
+    }
 }
