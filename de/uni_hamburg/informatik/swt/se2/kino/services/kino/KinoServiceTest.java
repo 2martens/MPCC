37f54730c573862c53ffb70cb778a7c84067773b
Jim Martens <jim1@live.de>
2013-06-24 21:09:14 +0200

    Added KinoService and related classes.

---
+++ de/uni_hamburg/informatik/swt/se2/kino/services/kino/KinoServiceTest.java
@@ -0,0 +1,177 @@
+package de.uni_hamburg.informatik.swt.se2.kino.services.kino;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Datum;
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.FSK;
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Reinigungszeit;
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Uhrzeit;
+import de.uni_hamburg.informatik.swt.se2.kino.materialien.Film;
+import de.uni_hamburg.informatik.swt.se2.kino.materialien.Kino;
+import de.uni_hamburg.informatik.swt.se2.kino.materialien.Kinosaal;
+import de.uni_hamburg.informatik.swt.se2.kino.materialien.Vorstellung;
+import de.uni_hamburg.informatik.swt.se2.kino.materialien.Werbeblock;
+
+/**
+ * Testklasse für KinoService.
+ * 
+ * @author Jim Martens
+ * @version 24.06.2013
+ * @copyright 2013 Jim Martens
+ */
+public class KinoServiceTest
+{
+    private KinoService _kinoService;
+    private Kino _kino;
+    
+    private Film _filmVorhanden;
+    private Film _filmVorhanden2;
+    private Film _filmVorhanden3;
+    private Film _filmNichtVorhanden;
+    private Vorstellung _vorstellungVorhanden;
+    private Vorstellung _vorstellungNichtVorhanden;
+    private Vorstellung _vorstellungNichtHinzufuegbar;
+    private Kinosaal _kinosaal1;
+    private Kinosaal _kinosaal2;
+    private Uhrzeit _1130;
+    private Uhrzeit _1500;
+    private Uhrzeit _1730;
+    private Uhrzeit _2230;
+    private Datum _heute;
+    private Werbeblock _blockKlein;
+    private Werbeblock _blockGross;
+    private Reinigungszeit _zeitKlein;
+    private Reinigungszeit _zeitGross;
+    
+    @Before
+    public void setUp() throws Exception
+    {
+        _1130 = new Uhrzeit(11, 30);
+        _1500 = new Uhrzeit(15, 00);
+        _1730 = new Uhrzeit(17, 30);
+        _2230 = new Uhrzeit(22, 30);
+        _heute = Datum.heute();
+        _blockKlein = new Werbeblock(15, FSK.FSK0);
+        _blockGross = new Werbeblock(30, FSK.FSK6);
+        _zeitKlein = new Reinigungszeit(15);
+        _zeitGross = new Reinigungszeit(30);
+        
+        _kinosaal1 = new Kinosaal("Kino A", 20, 20, _zeitKlein);
+        _kinosaal2 = new Kinosaal("Kino B", 30, 30, _zeitGross);
+        _filmVorhanden = new Film("Wilder Westen", 90, FSK.FSK18, false, false);
+        _filmVorhanden2 = new Film("Star Ship", 180, FSK.FSK12, true, false);
+        _filmVorhanden3 = new Film("Sommerliebe", 90, FSK.FSK12, false, false);
+        _filmNichtVorhanden = new Film("Lahmer Boden", 120, FSK.FSK6, false,
+                false);
+        _vorstellungVorhanden = new Vorstellung(_kinosaal1, _filmVorhanden3,
+                _1500, _heute, Vorstellung.TICKETPREIS,
+                _kinosaal1.getReinigungszeit(), _blockKlein);
+        _vorstellungNichtVorhanden = new Vorstellung(_kinosaal1,
+                _filmVorhanden2, _1730, _heute, Vorstellung.TICKETPREIS,
+                _kinosaal1.getReinigungszeit(), _blockGross);
+        _vorstellungNichtHinzufuegbar = new Vorstellung(_kinosaal1,
+                _filmVorhanden2, _1500, _heute, Vorstellung.TICKETPREIS,
+                _kinosaal1.getReinigungszeit(), _blockGross);
+        
+        _kino = new Kino(new Kinosaal[] { _kinosaal1 },
+                new Vorstellung[] { _vorstellungVorhanden }, new Film[] {
+                        _filmVorhanden, _filmVorhanden2 });
+        
+        _kinoService = new KinoServiceImpl(_kino);
+    }
+    
+    @Test
+    public void testIstFilmZeigenMoeglich()
+    {
+        // Endzeit überschneidet sich mit Anfangszeit bereits vorhandener
+        // Vorstellung
+        boolean result = _kinoService.istFilmZeigenMoeglich(_filmVorhanden2,
+                _blockGross, _zeitKlein, _kinosaal1, _heute, _1130);
+        assertFalse(result);
+        
+        // Anfangszeit ist nach Endzeit vorangehender Vorstellung, daher kein
+        // Problem
+        result = _kinoService.istFilmZeigenMoeglich(_filmVorhanden2,
+                _blockGross, _zeitKlein, _kinosaal1, _heute, _1730);
+        assertTrue(result);
+        
+        // Filme mit FSK 18 dürfen erst ab 22:00 gezeigt werden
+        result = _kinoService.istFilmZeigenMoeglich(_filmVorhanden,
+                _blockKlein, _zeitKlein, _kinosaal1, _heute, _1730);
+        assertFalse(result);
+        
+        result = _kinoService.istFilmZeigenMoeglich(_filmVorhanden,
+                _blockGross, _zeitKlein, _kinosaal1, _heute, _2230);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testFuegeVorstellungHinzu()
+    {
+        assertFalse(_kinoService
+                .istVorstellungVorhanden(_vorstellungNichtVorhanden));
+        _kinoService.fuegeVorstellungHinzu(_vorstellungNichtVorhanden, _heute,
+                _kinosaal1, _1730);
+        assertTrue(_kinoService
+                .istVorstellungVorhanden(_vorstellungNichtVorhanden));
+    }
+    
+    @Test
+    public void testIstHinzufuegenMoeglich()
+    {
+        assertFalse(_kinoService.istHinzufuegenMoeglich(_vorstellungVorhanden));
+        
+        // Vorstellung nicht vorhanden und keine zeitlichen/räumlichen
+        // Überschneidungen
+        assertTrue(_kinoService
+                .istHinzufuegenMoeglich(_vorstellungNichtVorhanden));
+        
+        // Vorstellung nicht vorhanden, aber zeitliche und räumliche
+        // Überschneidung mit vorhandener Vorstellung
+        assertFalse(_kinoService
+                .istHinzufuegenMoeglich(_vorstellungNichtHinzufuegbar));
+    }
+    
+    @Test
+    public void testIstVorstellungVorhanden()
+    {
+        assertTrue(_kinoService.istVorstellungVorhanden(_vorstellungVorhanden));
+        assertFalse(_kinoService
+                .istVorstellungVorhanden(_vorstellungNichtVorhanden));
+    }
+    
+    @Test
+    public void testEntferneVorstellung()
+    {
+        assertTrue(_kinoService.istVorstellungVorhanden(_vorstellungVorhanden));
+        _kinoService.entferneVorstellung(_vorstellungVorhanden);
+        assertFalse(_kinoService.istVorstellungVorhanden(_vorstellungVorhanden));
+    }
+    
+    @Test
+    public void testGetVorstellung()
+    {
+        Vorstellung v = _kinoService.getVorstellung(_kinosaal1, _heute, _1500);
+        assertSame(_vorstellungVorhanden, v);
+    }
+    
+    @Test
+    public void testIstKinosaalVorhanden()
+    {
+        assertTrue(_kinoService.istKinosaalVorhanden(_kinosaal1));
+        assertFalse(_kinoService.istKinosaalVorhanden(_kinosaal2));
+    }
+    
+    @Test
+    public void testIstFilmVorraetig()
+    {
+        assertTrue(_kinoService.istFilmVorraetig(_filmVorhanden));
+        assertFalse(_kinoService.istFilmVorraetig(_filmNichtVorhanden));
+    }
+    
+}
